a1e899a5d58c3fd9db412c59249965d3e57232dd
void4 <void4@noreply.github.com>
2022-12-20 01:58:38 +0100

    Put datastructure into separate file

---
+++ datastructures.py
@@ -0,0 +1,202 @@
+from collections import Counter
+
+class Node:
+	def __init__(self, qw, qh, level, parent=None, path=None, root=None):
+		self.qw = qw
+		self.qh = qh
+		if path is None:
+			self.path = []
+		else:
+			self.path = path
+		self.parent = parent
+		self.offset = 0
+		self.level = level
+		if root is None:
+			self.root = self
+		else:
+			self.root = root
+		self.counter = Counter({0:4**level*self.qw*self.qh})
+		if level == 0:
+			self.map = [[0 for x in range(self.qw)] for y in range(self.qh)]
+			self.sx = 0
+			self.sy = 0
+			for i, v in enumerate(self.path):
+				xx = v%2
+				yy = v//2
+				self.sx += 2**(len(self.path)-i)*self.qw//2*xx
+				self.sy += 2**(len(self.path)-i)*self.qw//2*yy
+			#print(self.path, self.sx, self.sy)
+		else:
+			self.children = []
+			for i in range(4):
+				self.children.append(Node(qw, qh, level-1, self, self.path+[i], self.root))
+	
+	def index(self, x, y):
+		scale = 2**self.level
+		xres = scale*self.qw // 2
+		yres = scale*self.qh // 2
+		cx = x//xres
+		cy = y//yres
+		index = 2*cy+cx
+		nx = x-xres*cx
+		ny = y-yres*cy
+		return index, nx, ny
+	
+	def get(self, x, y):
+		if self.level == 0:
+			return self.map[y][x]
+		else:
+			index, nx, ny = self.index(x,y)
+			return self.children[index].get(nx, ny)
+
+	def set(self, x, y, v):
+		if self.level == 0:
+			old = self.map[y][x]
+			self.counter[old] -= 1
+			self.counter[v] += 1
+			self.map[y][x] = v
+			return old
+		else:
+			index, nx, ny = self.index(x,y)
+			old = self.children[index].set(nx, ny, v)
+			self.counter[old] -= 1
+			self.counter[v] += 1			
+			return old
+	
+	def getQuad(self, path):
+		if path is None:
+			return None
+		elif len(path) == 0:
+			return self
+		else:
+			return self.children[path[0]].getQuad(path[1:])
+	
+	def pathmath(self, path, delta):
+		#print(path, delta)
+		if path == []:
+			# failure, out of bounds
+			return None
+		# only support single 0/1-deltas for now
+		dx = delta[0]
+		dy = delta[1]
+		last = path[-1]
+		lx = last%2
+		ly = last//2
+		nx = lx+dx
+		ny = ly+dy
+		if nx == -1:
+			subpath = self.pathmath(path[:-1], (-1,0))
+			return subpath + [1+2*ny] if subpath else None
+		elif nx == 2:
+			subpath = self.pathmath(path[:-1], (1,0))
+			return subpath + [0+2*ny] if subpath else None
+		elif ny == -1:
+			subpath = self.pathmath(path[:-1], (0,-1))
+			return subpath + [nx+2*1] if subpath else None
+		elif ny == 2:
+			subpath = self.pathmath(path[:-1], (0,1))
+			return subpath + [nx+2*0] if subpath else None
+		else:
+			return path[:-1] + [nx+ny*2]
+			
+	
+	def getRelativeQuad(self, path, delta):
+		newpath = self.pathmath(path, delta)
+		return self.root.getQuad(newpath)
+	
+	def allCoordDeltas(self, target):
+		for y in range(self.qh):
+			for x in range(self.qw):
+				if self.map[y][x] == target:
+					for delta in [(0,1), (0,-1), (1,0), (-1,0)]:
+						yield x, y, delta
+	
+	def borderDeltas(self, target):
+		#for caching efficiency
+		for x in range(self.qw):
+			if self.map[0][x] == target:
+				yield x, 0, (0, -1)
+		for x in range(self.qw):
+			if self.map[self.qh-1][x] == target:
+				yield x, self.qh-1, (0, 1)
+		for y in range(self.qh):
+			if self.map[y][0] == target:
+				yield 0, y, (-1, 0)
+		for y in range(self.qh):
+			if self.map[y][self.qw-1] == target:
+				yield self.qw-1, y, (1, 0)
+	
+	def getBorderTo(self, a, condition, count=None, border=None, mustcontain=None):
+		if border is None:
+			border = set()
+		# could improve efficiency with intermediate stages
+		if self.level == 0:
+			if a not in self.counter:
+				return border
+			
+			# TODO if only a and 0 in count, only check siblings, outer border
+			
+			codeltagen = self.allCoordDeltas(a) if mustcontain is None or mustcontain in self.counter else self.borderDeltas(a)
+			
+			for x, y, delta in codeltagen:
+			
+				if count is not None and len(border) >= count:
+					return border
+			
+				nx = x + delta[0]
+				ny = y + delta[1]
+				
+				# TODO
+				within = False
+				if nx < 0:
+					#	# check sibling, difficult in quadtree, may have to go several parents up, then down again
+					#	self.parent.children[
+					nx = self.qw-1
+					ny = y
+				elif ny < 0:
+					nx = x
+					ny = self.qh-1
+				elif nx >= self.qw:
+					nx = 0
+					ny = y
+				elif ny >= self.qh:
+					nx = x
+					ny = 0
+				else:
+					within = True
+					# within this quad
+					if condition(self.map[ny][nx]):
+						border.add((self.sx+nx, self.sy+ny))
+				
+				if not within:
+					# TODO: cache this!
+					sibling = self.root.getRelativeQuad(self.path, delta)
+					if sibling is not None:
+						if condition(sibling.map[ny][nx]):
+							border.add((sibling.sx+nx, sibling.sy+ny))
+			return border
+		else:
+			if a in self.counter:
+				for child in self.children:
+					border = child.getBorderTo(a, condition, count, border, mustcontain)
+				return border
+			else:
+				return border
+
+	def getFullCount(self):
+		return self.counter
+
+	def getFullBaseCount(self):
+		if self.level == 0:
+			return self.counter
+		else:
+			return sum([child.getFullCount() for child in self.children], Counter())
+
+if __name__ == "__main__":
+	QW = QH = 32
+	root = Node(QW, QH, 4)
+	print(root.get(0,0))
+	root.set(0,0,1)
+	print(root.get(0,0))
+	print(root.getFullCount())
+	print(root.getBorderTo(1,0))
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from collections import defaultdict, Counter
 from time import sleep
 from random import random, randint
+from math import log, ceil
 from copy import deepcopy
 
 from PIL import Image
@@ -10,6 +11,8 @@ import numpy as np
 import contextlib
 with contextlib.redirect_stdout(None):
     import pygame
+
+from datastructures import Node
 
 fontcache = {}
 
@@ -43,203 +46,13 @@ class Player:
 	def play(self):
 		pass
 
+def nextPowerOfTwo(v):
+	power = 1
+	while power < v:
+		power *= 2
+	return power
+
 QW = QH = 32
-
-class Node:
-	def __init__(self, level, parent=None, path=None, root=None):
-		if path is None:
-			self.path = []
-		else:
-			self.path = path
-		self.parent = parent
-		self.offset = 0
-		self.level = level
-		if root is None:
-			self.root = self
-		else:
-			self.root = root
-		self.counter = Counter({0:4**level*QW*QH})
-		if level == 0:
-			self.map = [[0 for x in range(QW)] for y in range(QH)]
-			self.sx = 0
-			self.sy = 0
-			for i, v in enumerate(self.path):
-				xx = v%2
-				yy = v//2
-				self.sx += 2**(len(self.path)-i)*QW//2*xx
-				self.sy += 2**(len(self.path)-i)*QW//2*yy
-			#print(self.path, self.sx, self.sy)
-		else:
-			self.children = []
-			for i in range(4):
-				self.children.append(Node(level-1, self, self.path+[i], self.root))
-	
-	def index(self, x, y):
-		scale = 2**self.level
-		xres = scale*QW // 2
-		yres = scale*QH // 2
-		cx = x//xres
-		cy = y//yres
-		index = 2*cy+cx
-		nx = x-xres*cx
-		ny = y-yres*cy
-		return index, nx, ny
-	
-	def get(self, x, y):
-		if self.level == 0:
-			return self.map[y][x]
-		else:
-			index, nx, ny = self.index(x,y)
-			return self.children[index].get(nx, ny)
-
-	def set(self, x, y, v):
-		if self.level == 0:
-			old = self.map[y][x]
-			self.counter[old] -= 1
-			self.counter[v] += 1
-			self.map[y][x] = v
-			return old
-		else:
-			index, nx, ny = self.index(x,y)
-			old = self.children[index].set(nx, ny, v)
-			self.counter[old] -= 1
-			self.counter[v] += 1			
-			return old
-	
-	def getQuad(self, path):
-		if path is None:
-			return None
-		elif len(path) == 0:
-			return self
-		else:
-			return self.children[path[0]].getQuad(path[1:])
-	
-	def pathmath(self, path, delta):
-		#print(path, delta)
-		if path == []:
-			# failure, out of bounds
-			return None
-		# only support single 0/1-deltas for now
-		dx = delta[0]
-		dy = delta[1]
-		last = path[-1]
-		lx = last%2
-		ly = last//2
-		nx = lx+dx
-		ny = ly+dy
-		if nx == -1:
-			subpath = self.pathmath(path[:-1], (-1,0))
-			return subpath + [1+2*ny] if subpath else None
-		elif nx == 2:
-			subpath = self.pathmath(path[:-1], (1,0))
-			return subpath + [0+2*ny] if subpath else None
-		elif ny == -1:
-			subpath = self.pathmath(path[:-1], (0,-1))
-			return subpath + [nx+2*1] if subpath else None
-		elif ny == 2:
-			subpath = self.pathmath(path[:-1], (0,1))
-			return subpath + [nx+2*0] if subpath else None
-		else:
-			return path[:-1] + [nx+ny*2]
-			
-	
-	def getRelativeQuad(self, path, delta):
-		newpath = self.pathmath(path, delta)
-		return self.root.getQuad(newpath)
-	
-	def allCoordDeltas(self, target):
-		print("all")
-		for y in range(QH):
-			for x in range(QW):
-				if self.map[y][x] == target:
-					for delta in [(0,1), (0,-1), (1,0), (-1,0)]:
-						yield x, y, delta
-	
-	def borderDeltas(self, target):
-		print("border")
-		#for caching efficiency
-		for x in range(QW):
-			if self.map[0][x] == target:
-				yield x, 0, (0, -1)
-		for x in range(QW):
-			if self.map[QH-1][x] == target:
-				yield x, QH-1, (0, 1)
-		for y in range(QH):
-			if self.map[y][0] == target:
-				yield 0, y, (-1, 0)
-		for y in range(QH):
-			if self.map[y][QW-1] == target:
-				yield QW-1, y, (1, 0)
-	
-	def getBorderTo(self, a, b):
-		# could improve efficiency with intermediate stages
-		if self.level == 0:
-			if a not in self.counter:
-				return set()
-			
-			# TODO if only a and 0 in count, only check siblings, outer border
-			border = set()
-			
-			codeltagen = self.allCoordDeltas(a) if b in self.counter else self.borderDeltas(a)
-			
-			for x, y, delta in codeltagen:
-				nx = x + delta[0]
-				ny = y + delta[1]
-				
-				# TODO
-				within = False
-				if nx < 0:
-					#	# check sibling, difficult in quadtree, may have to go several parents up, then down again
-					#	self.parent.children[
-					nx = QW-1
-					ny = y
-				elif ny < 0:
-					nx = x
-					ny = QH-1
-				elif nx >= QW:
-					nx = 0
-					ny = y
-				elif ny >= QH:
-					nx = x
-					ny = 0
-				else:
-					within = True
-					# within this quad
-					if self.map[ny][nx] == b:
-						border.add((self.sx+nx, self.sy+ny))
-				
-				if not within:
-					# TODO: cache this!
-					sibling = self.root.getRelativeQuad(self.path, delta)
-					if sibling is not None:
-						if sibling.map[ny][nx] == b:
-							border.add((self.sx+nx, self.sy+ny))
-			return border
-		else:
-			if a in self.counter:
-				total = set()
-				for child in self.children:
-					total = total.union(child.getBorderTo(a,b))
-				return total
-			else:
-				return set()
-
-	def getFullCount(self):
-		return self.counter
-
-	def getFullBaseCount(self):
-		if self.level == 0:
-			return self.counter
-		else:
-			return sum([child.getFullCount() for child in self.children], Counter())
-
-root = Node(4)
-print(root.get(0,0))
-root.set(0,0,1)
-print(root.get(0,0))
-print(root.getFullCount())
-print(root.getBorderTo(1,0))
-exit(0)
 
 class World:
 	def __init__(self):
@@ -248,41 +61,47 @@ class World:
 
 		self.w, self.h = self.backgroundimage.size
 		print("loaded map with size", self.w, self.h)
-		self.ownership = [[0 for x in range(self.w)] for y in range(self.h)]
 		
+		pot = ceil(log(max(nextPowerOfTwo(self.w/QW), nextPowerOfTwo(self.h/QH)), 2))
+		print("pot", pot)
 		
-		self.tick = 0
-		
-		self.total_occupyable = 0
+		self.ownership = Node(QW, QH, pot)
 		
 		for y in range(self.h):
 			for x in range(self.w):
-				if self.occupyable(x, y):
-					self.total_occupyable += 1
-
+				if self.background[y][x][0] + self.background[y][x][1] - self.background[y][x][2] > 200:
+					# impassable
+					self.ownership.set(x,y,0)
+				else:
+					# occupyable
+					self.ownership.set(x,y,1)
+		
+		print(self.ownership.counter)
+		
+		self.tick = 0
+		
 		NUMPLAYERS = 100
 
 		self.players = {}
 		
-		for p in range(1, NUMPLAYERS+1):
-			pno = len(self.players)
+		for pno in range(2, NUMPLAYERS+2):
 			self.players[pno] = Player(pno)
 			
 			while True:
 				sx, sy = randint(0, self.w-1), randint(0, self.h-1)
-				if self.empty_occupyable(sx, sy):
-					self.ownership[sy][sx] = pno
+				if self.empty_occupyable(self.ownership.get(sx, sy)):
+					self.ownership.set(sx, sy, pno)
 					break
 
-	def occupyable(self, x, y):
-		return not np.array_equal(self.background[y][x], [0,0,0]) and self.background[y][x][0] + self.background[y][x][1] - self.background[y][x][2] > 100 
+	def occupyable(self, v):
+		return v != 0
 
-	def empty_occupyable(self, x, y):
-		return self.occupyable(x, y) and self.ownership[y][x] == 0
+	def empty_occupyable(self, v):
+		return v == 1
 
-	def conquerable(self, pid, x, y):
+	def conquerable(self, pid, v):
 		# TODO check adjacency?
-		return self.occupyable(x, y) and self.ownership[y][x] != pid and not self.friendly(pid, self.ownership[y][x])
+		return self.occupyable(v) and v != pid and not self.friendly(pid, v)
 
 	def friendly(self, pid1, pid2):
 		if pid1 == 0 or pid2 == 0:
@@ -291,100 +110,63 @@ class World:
 	
 	def conquerableFor(self, pid):
 
-		def inner(x, y):
-			return self.conquerable(pid, x, y)
+		def inner(v):
+			return self.conquerable(pid, v)
 
 		return inner
 	
 	def isPlayer(self, pno):
 
-		def inner(x, y):
-			return self.ownership[y][x] == pno
+		def inner(v):
+			return v == pno
 
 		return inner
 	
 	def getConquerableAdjacent(self, pno, count):
-		return self.getAdjacent(pno, count, condition=self.conquerableFor(pno))
+		return self.ownership.getBorderTo(pno, condition=self.conquerableFor(pno), count=count)
 
 	def getAdjacentEnemy(self, pno, enemy, count):
-		return self.getAdjacent(pno, count, condition=self.isPlayer(enemy), checkreverse=True)
+		return self.ownership.getBorderTo(pno, condition=self.isPlayer(enemy), count=count, mustcontain=enemy)
 	
 	def getFreeAdjacent(self, pno, count):
-		return self.getAdjacent(pno, count, condition=self.empty_occupyable, checkreverse=True)
-	
-	def getAdjacent(self, pno, count, condition, checkreverse=False):
-		found = 0
-		adjacent = []
-		playerset = self.playersets[pno] if not checkreverse else reversed(self.playersets[pno])#should be a bit faster, because newer fields (less likely to be completely surrounded by own ones) are checked first, unless array is copied
-		for x,y in playerset:
-			for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
-				nx, ny = x+dx, y+dy
-				if nx < 0 or ny < 0 or nx >= self.w or ny >= self.h:
-					continue
-				
-				if condition(nx, ny):
-					adjacent.append([nx,ny])
-				
-			if found == count:
-				return adjacent
-
-		return adjacent
+		return self.ownership.getBorderTo(pno, condition=self.empty_occupyable, count=count)
 	
 	def update(self):
 
-
-		cnt = Counter()
-		
-		self.playersets = defaultdict(list)
-		
-		for y in range(self.h):
-			for x in range(self.w):
-				cnt[self.ownership[y][x]] += 1
-				self.playersets[self.ownership[y][x]].append([x,y])
-		"""
-		
-		unique, counts = np.unique(self.ownership, return_counts=True)
-		for pno, count in zip(unique, counts):
-			cnt[pno] = count
-			
-			self.playersets[pno] = np.argwhere(self.ownership==pno)#list(zip(*np.where(self.ownership==pno)))
-		"""
-		print(cnt)
 		
 		for pno, player in self.players.items():
-		
 			for army in player.armies:
-				if army.target != 0:
+				if army.target != 1:
 					attacking = int(army.strength*0.5)
 					beforeBalance = self.players[army.target].balance
 					self.players[army.target].balance -= attacking
-					lostTerritory = (attacking/beforeBalance)*len(self.playersets[army.target])
-					actualLost = 0
-					for x,y in self.getAdjacentEnemy(pno, army.target, lostTerritory):
-						actualLost += 1
-						self.ownership[y][x] = pno
-					print(army.target, "lost", actualLost, "to", pno)
+					enemy_strength = self.ownership.counter[army.target]
+					if enemy_strength > 0:
+						lostTerritory = (attacking/beforeBalance)*enemy_strength
+						actualLost = 0
+						for x,y in self.getAdjacentEnemy(pno, army.target, lostTerritory):
+							actualLost += 1
+							self.ownership.set(x, y, pno)
+						print(army.target, "lost", actualLost, "to", pno)
 				else:
 					attacking = int(army.strength*0.5)
 					for x,y in self.getFreeAdjacent(pno, attacking):
-						self.ownership[y][x] = pno
+						self.ownership.set(x, y, pno)
 			
 			# XXX
 			player.armies = []
 		
-			player.balance += cnt[pno]
+			player.balance += self.ownership.counter[pno]
 		
 		if self.tick > 0 and self.tick % 10 == 0:
 			for pno, player in self.players.items():
 				player.payInterest()
 		
 		
-			
-		
 		defeated = []
 		
 		for pno, player in self.players.items():
-			player.balance = min(150 * cnt[pno], player.balance)
+			player.balance = min(150 * self.ownership.counter[pno], player.balance)
 		
 			if player.balance <= 0:
 				defeated.append(pno)
@@ -392,15 +174,16 @@ class World:
 			if player.balance > 0 and random() < 0.1:
 				strength = randint(1, player.balance)
 				player.balance -= strength
-				player.armies.append(Army(0, strength))
+				player.armies.append(Army(1, strength))
 				
 			if player.balance > 0 and random() < 0.1:
 				conquerable_adjacent = world.getConquerableAdjacent(pno, 1)
 				if len(conquerable_adjacent) > 0:
-					cx, cy = conquerable_adjacent[0]
+					cx, cy = list(conquerable_adjacent)[0]
 					strength = randint(1, player.balance)
 					player.balance -= strength
-					player.armies.append(Army(self.ownership[cy][cx], strength))
+					#print("attacking", self.ownership.get(cx, cy))
+					player.armies.append(Army(self.ownership.get(cx, cy), strength))
 				
 					
 		self.tick += 1
@@ -409,8 +192,8 @@ class World:
 		bg = deepcopy(self.background)
 		for y in range(self.h):
 			for x in range(self.w):
-				owner = self.ownership[y][x]
-				if owner != 0:
+				owner = self.ownership.get(x, y)
+				if owner > 1:
 					bg[y][x] = deepcopy(self.players[owner].color)
 		
 		return bg
